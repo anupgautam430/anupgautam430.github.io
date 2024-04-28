d320a04afea77a87799761e77312c529f90550c7
anupgautam430 <134568764+anupgautam430@users.noreply.github.com>
2024-04-28 10:55:02 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true
+});
+
+function videoAnimation(){
+    var videocon = document.querySelector("#video-container");
+    var playbtn = document.querySelector("#play")
+    videocon.addEventListener("mouseenter", function(){
+        gsap.to(playbtn,{
+            scale: 1,
+            opacity: 1
+        })
+    })
+    videocon.addEventListener("mouseleave", function(){
+        gsap.to(playbtn,{
+            scale:0,
+            opacity:0
+        })
+    })
+    videocon.addEventListener("mousemove", function(dets){
+        gsap.to(playbtn,{
+            left: dets.x-70,
+            top:dets.y-50
+        })
+    })
+}
+videoAnimation()
+
+function textAnimate(){
+    gsap.from("#page1 h1",{
+        y:100,
+        opacity:0,
+        delay:0.3,
+        duration:0.9,
+        stagger:0.4
+       })
+       gsap.from("#page1 #video-container",{
+        scale:0.9,
+        opacity:0,
+        delay:1.3,
+        duration:0.5,
+        
+       })
+}
+textAnimate()
